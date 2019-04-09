9d13fbaf73da61f98c6a5e20b2dc05e58f08094a
Brian Seidl <brian.seidl@coxautoinc.com>
2019-04-09 16:15:42 -0400

    heroku take 13

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: daphne koda.asgi:application
+web: daphne koda.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
+worker: python manage.py runworker -v2
--- koda/asgi.py
+++ koda/asgi.py
@@ -1,12 +1,5 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
 import os
-import django
-from channels.routing import get_default_application
+import channels.asgi
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "koda.settings")
-django.setup()
-application = get_default_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat.settings")
+channel_layer = channels.asgi.get_channel_layer()
