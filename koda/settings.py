be8a642f987704e38bebd16ab121f5ba7665f405
Brian Seidl <brian.seidl@coxautoinc.com>
2019-04-09 11:54:54 -0400

    heroku take 2

--- koda/settings.py
+++ koda/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = ''
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
