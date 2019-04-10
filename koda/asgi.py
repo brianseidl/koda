873333a435b804e4a452a8462076c287ab9f2e4c
Brian Seidl <brian.seidl@coxautoinc.com>
2019-04-09 22:04:34 -0400

    heroku take 28

--- koda/asgi.py
+++ koda/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbox.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "koda.settings")
 django.setup()
 application = get_default_application()
