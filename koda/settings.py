923b771240363327b8f82c2681245887392bfca1
Brian Seidl <brian.seidl@coxautoinc.com>
2019-04-11 13:51:37 -0400

    got rid of debug mode

--- koda/settings.py
+++ koda/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iz9j&g&3si#0wb6=z(_ue$+at#8%#c-a06s93ny!9t=ma4k_#w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['koda-app.herokuapp.com', 'localhost']
 
@@ -139,6 +139,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            #"hosts": [('127.0.0.1', 6379)], #uncomment for local dev
         },
     },
 }
