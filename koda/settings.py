f39b2c9c746e00163dafd852749bc0bb26603ced
Brian Seidl <brian.seidl@coxautoinc.com>
2019-04-13 01:14:17 -0400

    fix settings for prod

--- koda/settings.py
+++ koda/settings.py
@@ -138,8 +138,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            #"hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-            "hosts": [('127.0.0.1', 6379)], #uncomment for local dev
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            #"hosts": [('127.0.0.1', 6379)], #uncomment for local dev
         },
     },
 }
