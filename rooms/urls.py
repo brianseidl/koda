8767855a4ee6146690be39842f3083a5ec8d7b26
Brian Seidl <brian.seidl@coxautoinc.com>
2019-05-10 15:26:09 -0400

    add direct messages

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
-from .views import BaseRoomView, DetailRoomView
+from .views import *
+
 app_name='rooms'
+
 urlpatterns = [
-    path('', BaseRoomView.as_view(), name='index'),
-    path('<int:room_id>/', DetailRoomView.as_view(), name='detail'),
+    path('', BaseView.as_view(), name='homepage'),
+    path('rooms/', BaseRoomView.as_view(), name='base_room'),
+    path('chats/', BaseChatView.as_view(), name='base_chat'),
+    path('rooms/<int:room_id>/', DetailRoomView.as_view(), name='detail_room'),
+    path('chats/<int:chat_id>/', DetailChatView.as_view(), name='detail_chat'),
 ]
